339f2c2295cf87c769917930ef23dd9a143336e2
Volodymyr123rusofob <136646970+Volodymyr123rusofob@users.noreply.github.com>
2024-02-10 19:07:01 +0200

    task=redux_n

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 // import { PersistGate } from 'redux-persist/integration/react';
-import { persistor, store } from './redux/store';
+import { store } from './redux/store';
+// import { persistor} from './redux/store';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App';
 import './index.css';
